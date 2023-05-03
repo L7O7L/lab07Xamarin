15183cb512d8665c3ce172c525a2662e574fcf2e
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2023-05-03 10:42:12 -0500

    Add project files.

---
+++ Semana07A2023/Semana07A2023.Android/Implementations/TextToSpeechImplementation.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+
+using Semana07A2023.Interfaces;
+using System;
+using Android.Speech.Tts;
+
+namespace Semana07A2023.Droid.Implementations
+{
+    public class TextToSpeechImplementation : Java.Lang.Object, ITextToSpeech, TextToSpeech.IOnInitListener
+    {
+        public void OnInit([GeneratedEnum] OperationResult status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Speak(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
